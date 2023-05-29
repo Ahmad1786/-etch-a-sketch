159be0088b0bef487f576f7ea8078baf4244ee0c
Muhammad Ahmad <69284724+Ahmad1786@users.noreply.github.com>
2023-05-29 14:11:22 -0400

    start using eslint

--- file.js
+++ file.js
@@ -1,83 +1,92 @@
-let mainContainer = document.querySelector('.main-container');
-let numberOfSquaresPerSide = 16; //start with 16x16 grid by default
-fillGrid(numberOfSquaresPerSide); 
+/* eslint-disable no-return-assign */
+/* eslint-disable no-loop-func */
+/* eslint-disable no-alert */
+const mainContainer = document.querySelector('.main-container');
+let numberOfSquaresPerSide = 16; // start with 16x16 grid by default
+let eraserOn = false;
 
-// let eraserOn = false;
+function removeAllColorClasses(element) {
+  let className;
 
-const gridSizeButton = document.querySelector("#gridSizeButton");
-gridSizeButton.addEventListener('click', () => getUserChoice());
+  for (let i = 0; i < element.classList.length; i++) {
+    className = element.classList.item(i);
 
-const resetButton = document.querySelector("#resetButton");
-resetButton.addEventListener('click', () => resetGrid());
-
-const eraserButton = document.querySelector("#eraserButton");
-eraserButton.addEventListener('click', () => becomeEraser());
-
-
-function getUserChoice() {
-    userSizeChoice = +prompt("Chose Size of Grid if you wish");
-    numberOfSquaresPerSide = userSizeChoice;
-    if (!(userSizeChoice)) return;
-    removeGrid();
-    fillGrid(numberOfSquaresPerSide);
-}
-function resetGrid() {
-    let allSquares = document.querySelectorAll('.square');
-    allSquares.forEach(s => {
-        s.style.backgroundColor = '';
-        s.classList.remove('colored-red');
-    });
-
-}
-
-function becomeEraser() {
-    let allSquares = document.querySelectorAll('.square');
-    allSquares.forEach(s => {
-        s.addEventListener('mouseover', e => {
-            if (isMouseDown) {
-            e.target.classList.remove('colored-red')   
-            e.target.classList.add('colored-white')
-            }
-        });
-        s.addEventListener('mousedown', e => {
-            e.target.classList.remove('colored-red')   
-            e.target.classList.add('colored-white')
-        });
-    });
-}
-
-
-function removeGrid() {
-    while (mainContainer.firstChild) {
-        mainContainer.removeChild(mainContainer.firstChild);
+    if (className !== 'square') {
+      element.classList.remove(className);
+      i--;
     }
+  }
 }
 
-function fillGrid(numberOfSquaresPerSide) {
-   
-    let squareSize = mainContainer.clientWidth / numberOfSquaresPerSide;
+function fillGrid(numOfSquaresPerSide) {
+  const squareSize = mainContainer.clientWidth / numOfSquaresPerSide;
 
-    for (let i = 0; i < numberOfSquaresPerSide ** 2; i++) {
-    let squareDiv = document.createElement('div');
+  for (let i = 0; i < numOfSquaresPerSide ** 2; i++) {
+    const squareDiv = document.createElement('div');
     squareDiv.classList.add('square');
     squareDiv.style.width = `${squareSize}px`;
     squareDiv.style.height = `${squareSize}px`;
     mainContainer.appendChild(squareDiv);
-    
-    let isMouseDown = false; 
+
+    let isMouseDown = false;
     document.addEventListener('mousedown', () => isMouseDown = true);
     document.addEventListener('mouseup', () => isMouseDown = false);
-    
-    let allSquares = document.querySelectorAll('.square');
-    allSquares.forEach(s => {
-        s.addEventListener('mouseover', e => {
-            if (isMouseDown) {
-            e.target.classList.add('colored-red')
-            }
-        });
-        s.addEventListener('mousedown', e => {
-            e.target.classList.add('colored-red')
-        });
+
+    const allSquares = document.querySelectorAll('.square');
+    allSquares.forEach((s) => {
+      s.addEventListener('mouseover', (e) => {
+        if (isMouseDown) {
+          if (eraserOn) {
+            removeAllColorClasses(e.target);
+            e.target.classList.add('colored-white');
+          } else {
+            removeAllColorClasses(e.target);
+            e.target.classList.add('colored-red');
+          }
+        }
+      });
+      s.addEventListener('mousedown', (e) => {
+        if (eraserOn) {
+          removeAllColorClasses(e.target);
+          e.target.classList.add('colored-white');
+        } else {
+          removeAllColorClasses(e.target);
+          e.target.classList.add('colored-red');
+        }
+      });
     });
+  }
 }
+
+function removeGrid() {
+  while (mainContainer.firstChild) {
+    mainContainer.removeChild(mainContainer.firstChild);
+  }
 }
+
+function getUserChoice() {
+  const userSizeChoice = +prompt('Chose Size of Grid if you wish');
+  numberOfSquaresPerSide = userSizeChoice;
+  if (!(userSizeChoice)) return;
+  removeGrid();
+  fillGrid(numberOfSquaresPerSide);
+}
+function resetGrid() {
+  const allSquares = document.querySelectorAll('.square');
+  allSquares.forEach((s) => {
+    removeAllColorClasses(s);
+  });
+}
+
+fillGrid(numberOfSquaresPerSide);
+
+const gridSizeButton = document.querySelector('#gridSizeButton');
+gridSizeButton.addEventListener('click', () => getUserChoice());
+
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('click', () => resetGrid());
+
+const eraserButton = document.querySelector('#eraserButton');
+eraserButton.addEventListener('click', () => {
+  eraserOn = !eraserOn;
+});
